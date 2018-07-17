e868cd1c63af9d2e65fde09435624c7dca6135ec
zxn <1021012427@qq.com>
2018-07-17 11:13:53 +0800

    table encapsulation

---
+++ src/views/system/dict/dialog/detail.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-dialog :title="$t('table.detail')" width="560px" :visible.sync="visible" @close="onClose">
+    <el-form :model="form" label-width="70px" size="small">
+      <el-form-item :label="$t('dict.type')">
+        <el-input v-model="form.type" readonly></el-input>
+      </el-form-item>
+      <el-form-item :label="$t('dict.code')">
+        <el-input v-model="form.code" readonly></el-input>
+      </el-form-item>
+      <el-form-item :label="$t('dict.text')">
+        <el-input v-model="form.text" readonly></el-input>
+      </el-form-item>
+      <el-form-item :label="$t('dict.value')">
+        <el-input v-model="form.value" readonly></el-input>
+      </el-form-item>
+      <el-form-item :label="$t('dict.downLoad')">
+        <el-radio v-model="form.downLoad" label="1" :disabled="true">{{$t('dict.enum.downLoad.1')}}</el-radio>
+        <el-radio v-model="form.downLoad" label="2" :disabled="true">{{$t('dict.enum.downLoad.2')}}</el-radio>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script>
+  import { vsprintf } from 'sprintf-js/dist/sprintf.min.js'
+  import { Loading } from 'element-ui'
+
+  export default {
+    data() {
+      var form = JSON.parse(JSON.stringify(this.$parent.selectedRow));
+      return {
+        visible: true,
+        form: form
+      };
+    },
+    methods: {
+      onClose(){
+        this.$router.push("/");
+      }
+    },
+    mounted: function(){
+      console.log(this);
+      let loading = Loading.service({ target: this.$el.childNodes[0] });
+    }
+  };
+</script>
